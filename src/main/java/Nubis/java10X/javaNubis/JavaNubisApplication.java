9a0961d2f01f13e813760b123e79d553bfcee0ec
Brutos2324 <fvlbrutos212@gmail.com>
2025-05-05 18:21:18 -0300

    Um pouco de Spring-Boot

---
+++ src/main/java/Nubis/java10X/javaNubis/JavaNubisApplication.java
@@ -0,0 +1,13 @@
+package Nubis.java10X.javaNubis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNubisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaNubisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Nubis/java10X/javaNubis/JavaNubisApplicationTests.java
@@ -0,0 +1,13 @@
+package Nubis.java10X.javaNubis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaNubisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
